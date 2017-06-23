03312cebd319394c896c496f455615f6e688216f
mrkodssldrf <mrkodssldrf@gmail.com>
2017-06-23 13:13:15 +0200

    revert

--- src/cli/index.js
+++ src/cli/index.js
@@ -1,18 +1,28 @@
+const env = require('dotenv').config()
 const http = require('http').createServer()
 const socketIo = require('socket.io')(http)
 const os = require('os')
-
-
-
-socketIo.on('connection', s =>{
-
+console.log(process.env.LOCAL_SOCKET_SERVER)
+socketIo.on('connection', s => {
   s.on('connected', (d) => {
     s.emit('server.infos', {
-      os : [os.arch(), os.platform(), os.networkInterfaces(), os.hostname()],
-      user : s.handshake
+      os: [
+        os.arch(),
+        os.platform(),
+        os.networkInterfaces(),
+        os.hostname()
+      ],
+      user: s.handshake
     })
   })
-
   s.on('disconnect', () => console.log('gone'))
 })
-http.listen(3001, '10.0.0.2', () => console.log(http))
+console.log(process.env.LOCAL_SOCKET_DEV)
+if (process.env.LOCAL_SOCKET_DEV == "1") {
+  http.listen(process.env.LOCAL_SOCKET_PORT, process.env.LOCAL_SOCKET_SERVER_DEV, () => console.log(http))
+}
+else {
+  http.listen(process.env.LOCAL_SOCKET_PORT, process.env.LOCAL_SOCKET_SERVER, () => console.log(http))
+}
+
+
